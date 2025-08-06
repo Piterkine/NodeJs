7c8decb003a7ec66d337b590c151424f2a748191
Shashank Shukla <Shashankshukla192004@gmail>
2025-08-06 17:08:41 +0530

    Async,Sync in node js

---
+++ Basic-05/sync.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 20;
+
+function multiplyfun(a,b){
+
+          const result = a*b;
+          return result;
+
+}
+const c = multiplyfun(5,6)
+console.log("Multiplication result is : ", c);
+
+
